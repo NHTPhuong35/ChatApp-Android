262c5f813f265a730e01ae84a2aea3a21bc01387
huyhuy1712 <anhhuy17122004@gmail.com>
2025-03-13 16:47:34 +0700

    xong header

--- app/src/main/java/com/example/chatapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.java
@@ -1,4 +1,15 @@
 package com.example.chatapp.activities;
 
-public class ChatActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.chat_room);
+    }
 }
