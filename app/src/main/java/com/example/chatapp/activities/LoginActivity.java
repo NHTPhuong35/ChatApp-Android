ff960c929cd46c09d64863df3262fd40bbf8aa06
GDevNeON <neongaming4ever@gmail.com>
2025-03-20 14:46:16 +0700

    Long update login register ui, add snackbar notification

--- app/src/main/java/com/example/chatapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/activities/LoginActivity.java
@@ -1,20 +1,37 @@
 package com.example.chatapp.activities;
 
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.chatapp.R;
 import com.example.chatapp.controllers.UserController;
 import com.example.chatapp.models.User;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -22,25 +39,38 @@ import com.google.android.material.textfield.TextInputLayout;
 public class LoginActivity extends AppCompatActivity {
     private UserController userController;
     private TextView registerText;
+    private TextView snackbarText;
+    private ImageView snackbarIcon;
     private Button loginButton;
     private TextInputLayout emailTextLayout;
     private TextInputLayout passwordTextLayout;
     private TextInputEditText emailEditText;
     private TextInputEditText passwordEditText;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        View rootLayout = findViewById(R.id.login); // Thay R.id.root_layout bằng ID của layout gốc
+        checkDarkMode(this);
 
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+//                Trở về màn hình chính của điện thoại
+                finishAffinity();
+            }
+        });
+
+        View rootLayout = findViewById(R.id.login);
         rootLayout.setOnTouchListener((v, event) -> {
-            // Ẩn bàn phím và xóa focus khi chạm vào vùng trống
+//            Ẩn bàn phím và xóa focus khi chạm vào vùng trống
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 clearFocusAndHideKeyboard();
             }
-            return false; // Không chặn sự kiện, để các view khác hoạt động bình thường
+//            Không chặn sự kiện, để các view khác hoạt động bình thường
+            return false;
         });
 
         loginButton = findViewById(R.id.button_login);
@@ -55,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
         registerText.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
-            finish();
+            overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
         });
     }
 
@@ -67,22 +97,34 @@ public class LoginActivity extends AppCompatActivity {
         String email = String.valueOf(emailEditText.getText());
         String password = String.valueOf(passwordEditText.getText());
 
-        // Đăng nhập tài khoản
+//        Đăng nhập tài khoản
         if (checkInput(email, password)) {
             userController = new UserController();
             userController.loginUser(email, password, new UserController.LoginCallback() {
                 @Override
                 public void onSuccess(User user) {
+                    showCustomSnackbar(
+                            findViewById(android.R.id.content),
+                            "Đăng nhập thành công!",
+                            R.drawable.ic_check,
+                            getResources().getColor(R.color.green)
+                    );
+                    Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
+                    startActivity(intent);
+
                     Log.d("Login", "Đăng nhập thành công!");
                     Log.d("Login", user.getUserId() + " - " + user.getEmail() + " - " + user.getUsername() + " - " + user.getAvatarUrl());
-                    // Chuyển sang màn hình chính
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
                 }
 
                 @Override
                 public void onFailure(Exception e) {
+                    showCustomSnackbar(
+                            findViewById(android.R.id.content),
+                            "Đăng nhập thất bại!",
+                            R.drawable.ic_error,
+                            getResources().getColor(R.color.red)
+                    );
+
                     Log.e("Register", "Lỗi: " + e.getMessage());
                 }
             });
@@ -98,14 +140,13 @@ public class LoginActivity extends AppCompatActivity {
 
         boolean[] errors = {false, false};
 
-        // Kiểm tra rỗng
+//        Kiểm tra rỗng
         if (email.isBlank()) {
             emailTextLayout.setError("* Vui lòng nhập email");
             emailEditText.requestFocus();
             errors[0] = true;
         } else {
             emailTextLayout.setError(null);
-            errors[0] = false;
         }
         if (password.isBlank()) {
             passwordTextLayout.setError("* Vui lòng nhập mật khẩu");
@@ -113,10 +154,9 @@ public class LoginActivity extends AppCompatActivity {
             errors[1] = true;
         } else {
             passwordTextLayout.setError(null);
-            errors[1] = false;
         }
 
-        // Kiểm tra cú pháp email
+//        Kiểm tra cú pháp email
         if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
             emailTextLayout.setError("* Vui lòng nhập đúng định dạng email");
             emailEditText.requestFocus();
@@ -126,7 +166,7 @@ public class LoginActivity extends AppCompatActivity {
             errors[0] = false;
         }
 
-        // Kiểm tra độ dài mật khẩu
+//        Kiểm tra độ dài mật khẩu
         if (password.length() < 6) {
             passwordTextLayout.setError("* Mật khẩu phải có ít nhất 6 ký tự");
             passwordEditText.requestFocus();
@@ -142,18 +182,81 @@ public class LoginActivity extends AppCompatActivity {
 
         return !errors[0] && !errors[1];
     }
+    
+    private void checkDarkMode(Context context) {
+        ImageView loginIV = findViewById(R.id.image_login);
+
+        boolean isDarkMode = (context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+
+        Drawable bgLogin = ContextCompat.getDrawable(this, isDarkMode ? R.drawable.bg_login_dark : R.drawable.bg_login);
+
+        loginIV.setImageDrawable(bgLogin);
+    }
 
     private void clearFocusAndHideKeyboard() {
-        // Lấy View đang focus
+//        Lấy View đang focus
         View currentFocus = getCurrentFocus();
 
         if (currentFocus instanceof TextInputEditText) {
-            // Xóa focus khỏi TextInputEditText
+//            Xóa focus khỏi TextInputEditText
             currentFocus.clearFocus();
 
-            // Ẩn bàn phím
+//            Ẩn bàn phím
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
         }
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void showCustomSnackbar(View parentView, String textSnackbar, @DrawableRes int resId, @ColorInt int colorId) {
+        // Tạo Snackbar
+        Snackbar snackbar = Snackbar.make(parentView, "", Snackbar.LENGTH_INDEFINITE);
+
+        // Lấy view của Snackbar
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+        snackbarLayout.setPadding(0, 0, 0, 0); // Loại bỏ padding mặc định
+
+        // Inflate layout tùy chỉnh
+        LayoutInflater inflater = LayoutInflater.from(parentView.getContext());
+        View customView = inflater.inflate(R.layout.custom_snackbar, null);
+        snackbarText = customView.findViewById(R.id.snackbar_text);
+        snackbarText.setText(textSnackbar);
+        snackbarIcon = customView.findViewById(R.id.snackbar_icon);
+        snackbarIcon.setImageResource(resId);
+
+        // Thêm custom view vào snackbar
+        snackbarLayout.addView(customView, 0);
+
+        // Căn chỉnh vị trí của Snackbar ở cuối màn hình
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) snackbarLayout.getLayoutParams();
+        params.gravity = Gravity.TOP;
+        snackbarLayout.setLayoutParams(params);
+
+        // Hiển thị Snackbar
+        snackbar.show();
+
+        // Lấy ProgressBar từ layout
+        ProgressBar progressBar = customView.findViewById(R.id.snackbar_progress);
+        progressBar.setProgressTintList(ColorStateList.valueOf(colorId));
+
+        // Thiết lập ProgressBar chạy trong 3 giây
+        int duration = 3000;
+        ValueAnimator animator = ValueAnimator.ofInt(0, duration);
+        animator.setDuration(duration);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.addUpdateListener(animation -> {
+            int progress = (int) animation.getAnimatedValue();
+            progressBar.setProgress(progress);
+        });
+        animator.start();
+
+        // Handler để ẩn Snackbar sau 3 giây với hiệu ứng trượt ra ngoài
+        new Handler().postDelayed(() -> {
+            snackbar.getView().animate()
+                    .translationX(-snackbar.getView().getWidth()) // Trượt sang trái
+                    .setDuration(300) // Thời gian trượt
+                    .withEndAction(snackbar::dismiss) // Xóa Snackbar khi trượt xong
+                    .start();
+        }, duration);
     }
 }
--- app/src/main/java/com/example/chatapp/activities/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/activities/RegisterActivity.java
@@ -1,19 +1,37 @@
 package com.example.chatapp.activities;
 
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.chatapp.R;
 import com.example.chatapp.controllers.UserController;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,6 +40,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class RegisterActivity extends AppCompatActivity {
     private UserController userController;
     private TextView loginText;
+    private TextView snackbarText;
+    private ImageView snackbarIcon;
     private Button registerButton;
     private TextInputLayout usernameTextLayout;
     private TextInputLayout emailTextLayout;
@@ -32,18 +52,30 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText passwordEditText;
     private TextInputEditText repasswordEditText;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        View rootLayout = findViewById(R.id.register); // Thay R.id.root_layout bằng ID của layout gốc
+        checkDarkMode(this);
+
+//        Thao tác nhấn nút back để hủy all activity trước
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finishAffinity();
+            }
+        });
+
+        View rootLayout = findViewById(R.id.register);
         rootLayout.setOnTouchListener((v, event) -> {
-            // Ẩn bàn phím và xóa focus khi chạm vào vùng trống
+//            Ẩn bàn phím và xóa focus khi chạm vào vùng trống
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 clearFocusAndHideKeyboard();
             }
-            return false; // Không chặn sự kiện, để các view khác hoạt động bình thường
+//            Không chặn sự kiện, để các view khác hoạt động bình thường
+            return false;
         });
 
         registerButton = findViewById(R.id.button_register);
@@ -58,7 +90,7 @@ public class RegisterActivity extends AppCompatActivity {
         loginText.setOnClickListener(v -> {
             Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
             startActivity(intent);
-            finish();
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
         });
     }
 
@@ -77,20 +109,42 @@ public class RegisterActivity extends AppCompatActivity {
         String repassword = String.valueOf(repasswordEditText.getText());
         String avatarURL = null;
 
-        // Đăng ký tài khoản
+//        Đăng ký tài khoản
         if (checkInput(username, email, password, repassword)) {
             userController = new UserController();
             userController.registerUser(email, password, new UserController.RegisterCallback() {
                 @Override
                 public void onSuccess(FirebaseUser user) {
-                    Log.d("Register", "Đăng ký thành công: " + user.getEmail());
-                    // Cập nhật thông tin người dùng
+//                    Cập nhật thông tin người dùng
                     userController.updateUsername(user.getUid(), username);
                     userController.updateAvatar(user.getUid(), avatarURL);
+
+//                    Hiện thông báo snackbar
+                    showCustomSnackbar(
+                            findViewById(android.R.id.content),
+                            "Đăng ký thành công!",
+                            R.drawable.ic_check,
+                            getResources().getColor(R.color.green)
+                    );
+
+//                    Chuyển hướng đến màn hình login
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+
+                    Log.d("Register", "Đăng ký thành công: " + user.getEmail());
                 }
 
                 @Override
                 public void onFailure(String errorMessage) {
+//                    Hiện thông báo snackbar
+                    showCustomSnackbar(
+                            findViewById(android.R.id.content),
+                            "Đăng ký thất bại!",
+                            R.drawable.ic_error,
+                            getResources().getColor(R.color.red)
+                    );
+
                     Log.e("Register", "Lỗi: " + errorMessage);
                 }
             });
@@ -108,16 +162,15 @@ public class RegisterActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.edit_text_password);
         repasswordEditText = findViewById(R.id.edit_text_repassword);
 
-        boolean errors[] = {false, false, false, false};
+        boolean[] errors = {false, false, false, false};
 
-        // Kiểm tra rỗng
+//        Kiểm tra rỗng
         if (username.isBlank()) {
             usernameTextLayout.setError("* Vui lòng nhập tên người dùng");
             usernameEditText.requestFocus();
             errors[0] = true;
         } else {
             usernameTextLayout.setError(null);
-            errors[0] = false;
         }
         if (email.isBlank()) {
             emailTextLayout.setError("* Vui lòng nhập email");
@@ -125,7 +178,6 @@ public class RegisterActivity extends AppCompatActivity {
             errors[1] = true;
         } else {
             emailTextLayout.setError(null);
-            errors[1] = false;
         }
         if (password.isBlank()) {
             passwordTextLayout.setError("* Vui lòng nhập mật khẩu");
@@ -133,10 +185,9 @@ public class RegisterActivity extends AppCompatActivity {
             errors[2] = true;
         } else {
             passwordTextLayout.setError(null);
-            errors[2] = false;
         }
 
-        // Kiểm tra cú pháp email
+//        Kiểm tra cú pháp email
         if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
             emailTextLayout.setError("* Vui lòng nhập đúng định dạng email");
             emailEditText.requestFocus();
@@ -146,7 +197,7 @@ public class RegisterActivity extends AppCompatActivity {
             errors[1] = false;
         }
 
-        // Kiểm tra độ dài mật khẩu
+//        Kiểm tra độ dài mật khẩu
         if (password.length() < 6) {
             passwordTextLayout.setError("* Mật khẩu phải có ít nhất 6 ký tự");
             passwordEditText.requestFocus();
@@ -160,30 +211,91 @@ public class RegisterActivity extends AppCompatActivity {
             errors[2] = false;
         }
 
-        // Kiểm tra mật khẩu trùng khớp
+//        Kiểm tra mật khẩu trùng khớp
         if (!password.equals(repassword)) {
             repasswordTextLayout.setError("* Mật khẩu không trùng khớp");
             repasswordEditText.requestFocus();
             errors[3] = true;
         } else {
             repasswordTextLayout.setError(null);
-            errors[3] = false;
         }
 
         return !errors[0] && !errors[1] && !errors[2] && !errors[3];
     }
 
+    private void checkDarkMode(Context context) {
+        ImageView registerIV = findViewById(R.id.image_register);
+
+        boolean isDarkMode = (context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+
+        Drawable bg_register = ContextCompat.getDrawable(this, isDarkMode ? R.drawable.bg_register_dark : R.drawable.bg_register);
+
+        registerIV.setImageDrawable(bg_register);
+    }
+
     private void clearFocusAndHideKeyboard() {
-        // Lấy View đang focus
+//        Lấy View đang focus
         View currentFocus = getCurrentFocus();
 
         if (currentFocus instanceof TextInputEditText) {
-            // Xóa focus khỏi TextInputEditText
+//            Xóa focus khỏi TextInputEditText
             currentFocus.clearFocus();
 
-            // Ẩn bàn phím
+//            Ẩn bàn phím
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
         }
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void showCustomSnackbar(View parentView, String textSnackbar, @DrawableRes int resId, @ColorInt int colorId) {
+        // Tạo Snackbar
+        Snackbar snackbar = Snackbar.make(parentView, "", Snackbar.LENGTH_INDEFINITE);
+
+        // Lấy view của Snackbar
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+        snackbarLayout.setPadding(0, 0, 0, 0); // Loại bỏ padding mặc định
+
+        // Inflate layout tùy chỉnh
+        LayoutInflater inflater = LayoutInflater.from(parentView.getContext());
+        View customView = inflater.inflate(R.layout.custom_snackbar, null);
+        snackbarText = customView.findViewById(R.id.snackbar_text);
+        snackbarText.setText(textSnackbar);
+        snackbarIcon = customView.findViewById(R.id.snackbar_icon);
+        snackbarIcon.setImageResource(resId);
+
+        // Thêm custom view vào snackbar
+        snackbarLayout.addView(customView, 0);
+
+        // Căn chỉnh vị trí của Snackbar ở cuối màn hình
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) snackbarLayout.getLayoutParams();
+        params.gravity = Gravity.TOP;
+        snackbarLayout.setLayoutParams(params);
+
+        // Hiển thị Snackbar
+        snackbar.show();
+        // Lấy ProgressBar từ layout
+        ProgressBar progressBar = customView.findViewById(R.id.snackbar_progress);
+        progressBar.setProgressTintList(ColorStateList.valueOf(colorId));
+
+        // Thiết lập ProgressBar chạy trong 3 giây
+        int duration = 3000;
+        ValueAnimator animator = ValueAnimator.ofInt(0, duration);
+        animator.setDuration(duration);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.addUpdateListener(animation -> {
+            int progress = (int) animation.getAnimatedValue();
+            progressBar.setProgress(progress);
+        });
+        animator.start();
+
+        // Handler để ẩn Snackbar sau 3 giây với hiệu ứng trượt ra ngoài
+        new Handler().postDelayed(() -> {
+            snackbar.getView().animate()
+                    .translationX(-snackbar.getView().getWidth()) // Trượt sang trái
+                    .setDuration(300) // Thời gian trượt
+                    .withEndAction(snackbar::dismiss) // Xóa Snackbar khi trượt xong
+                    .start();
+        }, duration);
     }
 }
